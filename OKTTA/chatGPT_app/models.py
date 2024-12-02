dde4ea08b19a81718c305ee3155430348536bb69
dmitriy <kosheld@inbox.ru>
2024-12-02 20:06:44 +0300

    add chatGPT_app

---
+++ OKTTA/chatGPT_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class ChatGPT(models.Model):
+    CHOICES = (
+        ('company', 'Company'),
+        ('services', 'Services'),
+        ('regulations', 'Regulations'),
+    )
+
+    title = models.CharField(max_length=255, choices=CHOICES)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    instruction_file = models.FileField(upload_to='instructions/', null=True, blank=True)
+    user = models.ForeignKey('user_app.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
